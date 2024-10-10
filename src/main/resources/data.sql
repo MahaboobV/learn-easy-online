e7777b645c0552b6bc13f8ab33a1de3595b52a32
Mahaboob Monutty Veettilayil <MahaboobMonutty.Veettilayil@mastercardpaymentservices.com>
2024-10-11 01:57:09 +0200

    H2: Schema and data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO course (title, description, instructor, duration, course_type, price) VALUES
+('Introduction to Java', 'Learn the basics of Java programming.', 'John Doe', '4 weeks', 'Free', 0.0),
+('Web Development with Spring Boot', 'Build web applications using Spring Boot.', 'Jane Smith', '6 weeks', 'Paid', 49.99),
+('Data Science with Python', 'Analyze data and build models using Python.', 'Michael Brown', '8 weeks', 'Paid', 100.00);
